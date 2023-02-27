f2bd3a52d3f606bda8ee5d4599261ca86438b51d
Diana Jordan <dianacjordan@gmail.com>
2023-02-26 21:21:15 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form')
+
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        //2 ways to grab the form input
+        /*   
+          console.log(e.target.children[1].value) */
+        const input = document.querySelector("input#searchByID")
+
+
+        // passes value of the form input into the fetch
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(resp => resp.json())
+            .then(data => {
+                const title = document.querySelector('section#movieDetails h4')
+                const summary = document.querySelector('section#movieDetails p')
+                title.textContent = data.title
+                summary.textContent = data.summary
+            })
+    })
 }
+
 
 document.addEventListener('DOMContentLoaded', init);
